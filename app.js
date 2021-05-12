df986a9e51809f31099c93d1908f6744314d6de1
talhajjaj <taima.hajaj@gmail.com>
2021-05-12 18:17:56 +0300

    editing lab01b

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+alert ('welcome')
+let userName = prompt('please enter your name ?');
+console.log(userName);
+alert ('welcome '+userName);
+
+let userAge=prompt("How old are you?");
+console.log(userAge);
+alert( 'we are happy to know that ');
+let petName=prompt('whats your pet name?')
+console.log(petName);
+alert('Aw thats cute ');
+let familyName=prompt('please write your family name?')
+console.log(familyName);
+alert('its a pleaser to see you here ' + userName +  familyName);
+
+// let amount1 = 30
+// let amount2 = 20
+// console.log(amount1+amount2)
